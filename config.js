f2bb7261e0c6c2135bd1fbc060113fe1352854c6
Jack McCown <jmccown@cbinsights.com>
2018-06-09 18:23:29 -0400

    modify config passing

--- config.js
+++ config.js
@@ -1,21 +1,23 @@
 export default {
-  monitor: {
-    tests: [
-      'api/selenium-grid-dev',
-      'api/selenium-grid-staging',
-      'cbi-site/selenium-grid-dev',
-      'cbi-site/selenium-grid-staging'
-    ],
-    builds: [
-      'cbi-site/develop',
-      'cbi-site/release',
-      'cbi-site/master',
-      'cbi-api/develop',
-      'cbi-api/release',
-      // 'cbi-api/master',
-      'cbi/develop',
-      'cbi/release',
-      'cbi/master'
-    ]
-  }
+  jenkins: {
+    monitor: {
+      tests: [
+        'api/selenium-grid-dev',
+        'api/selenium-grid-staging',
+        'cbi-site/selenium-grid-dev',
+        'cbi-site/selenium-grid-staging',
+      ],
+      builds: [
+        'cbi-site/develop',
+        'cbi-site/release',
+        'cbi-site/master',
+        'cbi-api/develop',
+        'cbi-api/release',
+        // 'cbi-api/master',
+        'cbi/develop',
+        'cbi/release',
+        'cbi/master',
+      ],
+    },
+  },
 };
